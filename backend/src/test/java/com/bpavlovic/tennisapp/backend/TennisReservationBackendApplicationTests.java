37b834f2572dc649e76b849ead19d2276f435d70
Bruno Pavlović <bpavlovic20@foi.hr>
2025-05-15 20:25:22 +0200

    initial backend setup

---
+++ backend/src/test/java/com/bpavlovic/tennisapp/backend/TennisReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpavlovic.tennisapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
