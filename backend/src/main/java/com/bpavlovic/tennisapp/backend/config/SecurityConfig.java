5bf30aa376dca6a63afd926a6b9e31bc866521d3
Bruno Pavlović <bpavlovic20@foi.hr>
2025-07-29 17:54:48 +0200

    B - added hashing password

--- backend/src/main/java/com/bpavlovic/tennisapp/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/bpavlovic/tennisapp/backend/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,6 +17,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -45,5 +53,4 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
-
 }
--- backend/src/main/java/com/bpavlovic/tennisapp/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/bpavlovic/tennisapp/backend/mapper/UserMapper.java
@@ -2,18 +2,24 @@ package com.bpavlovic.tennisapp.backend.mapper;
 
 import com.bpavlovic.tennisapp.backend.dto.UserRegistrationDto;
 import com.bpavlovic.tennisapp.backend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 
 @Component
 public class UserMapper {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User toEntity(UserRegistrationDto userRegistrationDto){
         User user = new User();
         user.setFirstName(userRegistrationDto.getFirstName());
         user.setLastName(userRegistrationDto.getLastName());
         user.setEmail(userRegistrationDto.getEmail());
-        user.setPassword(userRegistrationDto.getPassword()); //for testing, need to hash it
+        user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
         user.setRole("USER");
         user.setCreditAmount(0.0);
         user.setCreatedAt(new Timestamp(System.currentTimeMillis()));
