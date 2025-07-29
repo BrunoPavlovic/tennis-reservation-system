7ef7e0f0053d02f12c10fff3cb2d44b28925a651
Bruno Pavlović <bpavlovic20@foi.hr>
2025-07-29 18:20:15 +0200

    B - added caching clubs

--- backend/src/main/java/com/bpavlovic/tennisapp/backend/TennisReservationBackendApplication.java
+++ backend/src/main/java/com/bpavlovic/tennisapp/backend/TennisReservationBackendApplication.java
@@ -2,8 +2,10 @@ package com.bpavlovic.tennisapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TennisReservationBackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/bpavlovic/tennisapp/backend/service/ClubService.java
+++ backend/src/main/java/com/bpavlovic/tennisapp/backend/service/ClubService.java
@@ -5,6 +5,7 @@ import com.bpavlovic.tennisapp.backend.mapper.ClubMapper;
 import com.bpavlovic.tennisapp.backend.model.Club;
 import com.bpavlovic.tennisapp.backend.repository.ClubRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class ClubService {
     private final ClubRepository clubRepository;
     private final ClubMapper clubMapper;
 
+    @Cacheable(value = "clubs")
     public List<ClubDto> getAllClubs() {
         List<Club> allClubs = clubRepository.findAll();
         return allClubs.stream()
