e03c9b05ef24c876a3dda5a2b1796927e3840c4a
Bruno Pavlović <bpavlovic20@foi.hr>
2025-08-12 15:33:47 +0200

    F - final Profile design

--- frontend/src/views/Profile.tsx
+++ frontend/src/views/Profile.tsx
@@ -7,19 +7,33 @@ import UserTransactions from "../components/UserTransactions";
 const Profile: React.FC = () => {
   return (
     <MDBContainer className="py-4">
-      <div className="row justify-content-center">
+      {/* Desktop Layout */}
+      <div className="row justify-content-center d-none d-lg-flex">
+        <div className="col-lg-4">
+          <UserInformation />
+        </div>
+        <div className="col-lg-8">
+          <UserReservations />
+          <div className="mt-4">
+            <UserTransactions />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Layout */}
+      <div className="row justify-content-center d-lg-none">
         <div className="col-12 col-lg-8 col-xl-6">
           <UserInformation />
         </div>
       </div>
       
-      <div className="row justify-content-center mt-4">
+      <div className="row justify-content-center mt-4 d-lg-none">
         <div className="col-12 col-lg-10 col-xl-8">
           <UserReservations />
         </div>
       </div>
       
-      <div className="row justify-content-center mt-4">
+      <div className="row justify-content-center mt-4 d-lg-none">
         <div className="col-12 col-lg-10 col-xl-8">
           <UserTransactions />
         </div>
